216220c1a5d17192fa41489eac7acca62d6293d1
cendanaaul <cendanaputria@gmail.com>
2023-12-11 20:08:16 +0700

    Initial commit

---
+++ app/src/main/java/com/cencen/bloommatecapstone/ui/detailproduct/DetailProductActivity.kt
@@ -0,0 +1,12 @@
+package com.cencen.bloommatecapstone.ui.detailproduct
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cencen.bloommatecapstone.R
+
+class DetailProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_product)
+    }
+}
---
+++ app/src/main/java/com/cencen/bloommatecapstone/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.cencen.bloommatecapstone.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cencen.bloommatecapstone.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
