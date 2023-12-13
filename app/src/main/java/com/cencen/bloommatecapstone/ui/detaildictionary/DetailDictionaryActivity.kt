ba144f17831317f3f96dc75d81bf9263a53881f7
Jessica Sianturi <sianturijessica7@gmail.com>
2023-12-13 12:50:23 +0700

    update splash

---
+++ app/src/main/java/com/cencen/bloommatecapstone/ui/detaildictionary/DetailDictionaryActivity.kt
@@ -0,0 +1,12 @@
+package com.cencen.bloommatecapstone.ui.detaildictionary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cencen.bloommatecapstone.R
+
+class DetailDictionaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_dictionary)
+    }
+}
---
+++ app/src/main/java/com/cencen/bloommatecapstone/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.cencen.bloommatecapstone.ui.register
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cencen.bloommatecapstone.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
